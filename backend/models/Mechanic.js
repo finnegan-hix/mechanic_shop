3d1fba82311d30d660e6dd11f5eb031ec88dfa82
Leonel-A-L <lamelas.leonel@gmail.com>
2023-07-08 00:03:50 -0400

    added mechanics model Schema

--- backend/models/Mechanic.js
+++ backend/models/Mechanic.js
@@ -1,15 +1,25 @@
-module.exports = (sequelize, DataTypes) => {
-    const Mechanic = sequelize.define(
-      "Mechanic",
-      {
-        name: DataTypes.STRING,
-        title: DataTypes.STRING,
-        doh: DataTypes.Date,
-      },
-      {}
-    );
-    Mechanic.associate = function(models) {
-      Mechanic.belongsToMany(models.Customer, { through: "Appointment", foreignKey: "mechanicId" });
-    };
-    return Mechanic;
-  };
+const mongoose = require('mongoose')
+
+const mechanicSchema = new mongoose.Schema({
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+        type:String,
+        required:true
+    },
+    title: {
+      type: String
+    },
+    dateOfHire:{
+        type:Date
+    },
+    image: {
+      type: String,
+      default: 'https://images.unsplash.com/photo-1615906655593-ad0386982a0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
+    },
+  
+})
+
+module.exports = mongoose.model('Mechanic', mechanicSchema)
